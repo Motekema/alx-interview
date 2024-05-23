0679c7a16a48df1fff692e847f4f063987e5cad3
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-05-23 13:11:43 +0200

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,31 +1,17 @@
 #!/usr/bin/node
+
 const request = require('request');
-const API_URL = 'https://swapi-api.hbtn.io/api';
-
-if (process.argv.length > 2) {
-  const movieId = process.argv[2];
-  request(`${API_URL}/films/${movieId}/`, (err, _, body) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    const charactersURL = JSON.parse(body).characters;
-    const charactersNamePromises = charactersURL.map(
-      url => new Promise((resolve, reject) => {
-        request(url, (promiseErr, __, charactersReqBody) => {
-          if (promiseErr) {
-            reject(promiseErr);
-            return;
-          }
-          resolve(JSON.parse(charactersReqBody).name);
-        });
-      })
-    );
-
-    Promise.all(charactersNamePromises)
-      .then(names => console.log(names.join('\n')))
-      .catch(allErr => console.error(allErr));
+// Function to fetch character names in the exact order
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
+});
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
   });
-} else {
-  console.log('Usage: ./0-starwars_characters.js <movie_id>');
-}
+};
