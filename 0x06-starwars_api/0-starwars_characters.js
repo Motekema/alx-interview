9c7566f93a29d3daaaf95cd882c67d8bad025732
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-05-23 16:44:43 +0200

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,7 +3,6 @@
 // Importing module for making HTTP requests
 const request = require('request');
 
-
 request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
   if (err) throw err;
   const actors = JSON.parse(body).characters;
