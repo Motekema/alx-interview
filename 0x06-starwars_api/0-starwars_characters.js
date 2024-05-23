5913d622c28170ee147e89be53d3889036fb7aa9
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-05-23 16:32:16 +0200

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,27 +1,18 @@
 #!/usr/bin/node
 
+// Importing module for making HTTP requests
 const request = require('request');
 
-// Function to fetch character names in the exact order
-const exactOrder = (actors, index) => {
-  if (index === actors.length) return; // Base case: if index equals the length of actors array, stop the recursion
-  request(actors[index], function (err, res, body) {
-    if (err) throw err; // If there's an error, throw the error
-    console.log(JSON.parse(body).name); // Parse and print the character's name
-    exactOrder(actors, index + 1); // Recursive call to fetch the next character
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
+});
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
   });
 };
-
-// Check if the Movie ID is provided as an argument
-if (process.argv.length > 2) {
-  const movieId = process.argv[2]; // Extract the Movie ID from the command line arguments
-
-  // Request the movie details from the Star Wars API using the provided Movie ID
-  request(`https://swapi-api.hbtn.io/api/films/${movieId}`, function (err, res, body) {
-    if (err) throw err; // If there's an error, throw the error
-    const actors = JSON.parse(body).characters; // Parse the response body to get the list of character URLs
-    exactOrder(actors, 0); // Call the exactOrder function to fetch and print character names in order
-  });
-} else {
-  console.log('Usage: ./0-starwars_characters.js <movie_id>'); // Display usage information if Movie ID is not provided
-}
