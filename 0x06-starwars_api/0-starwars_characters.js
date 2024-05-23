86af3f63f73316772e9807dfa6610e89000f9902
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-05-23 13:23:20 +0200

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,32 +1,17 @@
 #!/usr/bin/node
 
 const request = require('request');
-const movieId = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
 
-request(url, (err, res, body) => {
-  if (err) {
-    console.error('Error:', err);
-    return;
-  }
-
-  const film = JSON.parse(body);
-  const characters = film.characters;
-
-  const fetchCharacter = (index) => {
-    if (index >= characters.length) return;
-
-    request(characters[index], (err, res, body) => {
-      if (err) {
-        console.error('Error:', err);
-        return;
-      }
-
-      const character = JSON.parse(body);
-      console.log(character.name);
-      fetchCharacter(index + 1);
-    });
-  };
-
-  fetchCharacter(0);
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
 });
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
+  });
+};
