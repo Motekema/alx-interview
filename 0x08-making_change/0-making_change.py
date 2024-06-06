06f3a2c20c4fdbe63b9f75edba953616f3501dd6
Motekema <motekema@gmail.com>
2024-06-06 07:33:06 -0700

    My commit

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module for making change with minimum coins
+"""
+
+
+def makeChange(coins, total):
+    """
+    Returns the fewest number of coins needed to meet a given total amount
+    """
+    if total <= 0:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
+
+
+if __name__ == "__main__":
+    print(makeChange([1, 2, 25], 37))
+
